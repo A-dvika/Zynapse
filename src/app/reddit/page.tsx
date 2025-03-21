744fad642f8feaa1c435ebcc2a64f08ad9ecaa4e
kairveeehh <jampu741@gmail.com>
2025-03-21 14:06:03 +0530

    fixes

---
+++ src/app/reddit/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HackerNewsPage: React.FC = () => {
+    return (
+        <div>
+            <h1>reddit </h1>
+        </div>
+    );
+};
+
+export default HackerNewsPage;
